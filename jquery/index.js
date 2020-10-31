598170c902d4d9c370d026a57b677c188f2de6cd
Siddharth Rai <sidd35072@gmail.com>
2020-10-31 08:54:22 +0530

    Home page compleated

---
+++ jquery/index.js
@@ -0,0 +1,7 @@
+var titles = [
+    "Build Career and have fun",
+    "Redifing ourselves through rejection",
+    "A question of infinite answers",
+    "The golden habits",
+    "The life that I choose"
+]
