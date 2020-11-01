0fc105a8dcb5bfb88275895073de02a1a5362d89
Siddharth Rai <sidd35072@gmail.com>
2020-11-01 22:40:59 +0530

    Added animations

---
+++ jquery/animation.js
@@ -0,0 +1,37 @@
+function hiddd() {
+    var elements;
+    var windowHeight;
+  
+    function init() {
+      elements = document.querySelectorAll(".hidd");
+      windowHeight = window.innerHeight;
+    }
+  
+    function checkPosition() {
+        init();
+      for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var positionFromTop = elements[i].getBoundingClientRect().y;
+        console.log(positionFromTop);
+  
+        if (positionFromTop <= windowHeight / 1.3) {
+          console.log(element.classList);
+          if (element.classList.contains("aleft")) {
+            element.classList.add("fade-in-left");
+          } else if (element.classList.contains("aright")) {
+            element.classList.add("fade-in-right");
+          } else if (element.classList.contains("abottom")) {
+            element.classList.add("fade-in-bottom");
+          } else element.classList.add("fade-in-element");
+          element.classList.remove("hidd");
+        }
+      }
+    }
+  
+    window.addEventListener("scroll", checkPosition);
+    window.addEventListener("resize", init);
+  
+    init();
+    checkPosition();
+  }
+  hiddd();
